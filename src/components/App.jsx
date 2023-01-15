13d4f095ab0e9ad6a6c1f704559535bf6a516f96
Urszula Molska <urszula_molska@wp.pl>
2023-01-15 02:28:00 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchPictures } from './Api/Api.js';
 import { Searchbar } from './Searchbar/Searchbar.jsx';
 import { Button } from './Button/Button.jsx';
@@ -8,124 +8,103 @@ import { Modal } from './Modal/Modal.jsx';
 import { Loader } from './Loader/Loader.jsx';
 import { Section } from './Section/Section.jsx';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pictures: [],
-      isLoading: false,
-      search_term: '',
-      page: 1,
-      totalPages: 0,
-      per_page: 12,
-      modal: {
-        show: false,
-        img: '',
-        imgAlt: '',
-      },
-    };
-  }
+export const App = () => {
+  const [pictures, setPictures] = useState([]);
+  const [search_term, setSearch_term] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [per_page, setPer_page] = useState(12);
+  const [modal, setModal] = useState({ show: false, img: '', imgAlt: '' });
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
+    setPage(1);
     const form = event.target;
-    this.setState(
-      {
-        search_term: form.elements.searchQuery.value,
-        page: 1,
-      },
-      async () => {
-        this.setState({
-          isLoading: true,
-        });
-        const response = await fetchPictures(
-          this.state.search_term,
-          this.state.page,
-          this.state.per_page
-        );
-        this.setState({
-          pictures: response.hits,
-          isLoading: false,
-          totalPages: response.totalHits / this.state.per_page,
-        });
-      }
-    );
+    const search_value = form.elements.searchQuery.value;
+    setSearch_term(search_value);
+    fetchImages(search_term, page, per_page);
   };
 
-  loadMorePictures = () => {
-    this.setState(
-      prevState => {
-        return { page: prevState.page + 1 };
-      },
-      async () => {
-        await fetchPictures(
-          this.state.search_term,
-          this.state.page,
-          this.state.per_page
-        ).then(response => {
-          this.setState(prevState => ({
-            pictures: [...prevState.pictures, ...response.hits],
-          }));
-        });
-      }
-    );
+  const fetchImages = async (search_term, page, per_page) => {
+    setLoading(true);
+    await fetchPictures(search_term, page, per_page).then(response => {
+      setPictures(response.hits);
+      setTotalPages(response.totalHits / per_page);
+    });
+    setLoading(false);
   };
 
-  openModal = event => {
-    const { pictures } = this.state;
+  /*useEffect(() => {
+    fetchImages(search_term, page, per_page);
+  }, [search_term]);*/
+
+  //LOAD MORE PICTURES
+  const loadMorePictures = () => {
+    setPage(page + 1);
+
+    const fetchMorePictures = async (search_term, page, per_page) => {
+      await fetchPictures(search_term, page, per_page).then(response => {
+        setPictures([...pictures, ...response.hits]);
+      });
+    };
+    fetchMorePictures(search_term, page, per_page);
+    console.log(pictures, page, search_term);
+  };
+
+  const openModal = event => {
     event.preventDefault();
     const idForModal = event.currentTarget.id;
     const picture = pictures.find(
       picture => picture.id.toString() === idForModal
     );
-    this.setState({
-      modal: { show: true, img: picture.largeImageURL, imgAlt: picture.tags },
+    setModal({
+      show: true,
+      img: picture.largeImageURL,
+      imgAlt: picture.tags,
     });
   };
 
-  closeModal = event => {
-    this.setState({ modal: { show: false } });
+  const closeModal = event => {
+    setModal({ show: false });
   };
 
-  render() {
-    const { pictures, search_term, isLoading, modal } = this.state;
-    const showButton = this.state.totalPages > this.state.page;
+  const showButton = totalPages > page;
 
-    return (
-      <>
-        <Section>
-          <a href="https://urszula-molska.github.io/goit-react-hw-03-image-finder/">
-            https://urszula-molska.github.io/goit-react-hw-03-image-finder
-          </a>
-          <a href="https://github.com/Urszula-Molska/goit-react-hw-03-image-finder">
-            https://github.com/Urszula-Molska/goit-react-hw-03-image-finder
-          </a>
-        </Section>
-        <div className="container">
-          <Searchbar handleSubmit={this.handleSubmit} />
-        </div>
-        {isLoading === false ? (
-          <ImageGallery>
-            <ImageGalleryItem
-              pictures={pictures}
-              search_term={search_term}
-              openModal={this.openModal}
-            />
-          </ImageGallery>
-        ) : (
-          <div>
-            <Loader />
-          </div>
-        )}
-        {showButton && <Button loadMorePictures={this.loadMorePictures} />}
-        {modal.show && (
-          <Modal
-            closeModal={this.closeModal}
-            largeImage={modal.img}
-            alt={modal.imgAlt}
+  return (
+    <>
+      <Section>
+        <a href="https://urszula-molska.github.io/goit-react-hw-04-image-finder/">
+          https://urszula-molska.github.io/goit-react-hw-04-image-finder
+        </a>
+        <a href="https://github.com/Urszula-Molska/goit-react-hw-04-image-finder">
+          https://github.com/Urszula-Molska/goit-react-hw-04-image-finder
+        </a>
+      </Section>
+      <div className="container">
+        <Searchbar handleSubmit={handleSubmit} />
+      </div>
+      {isLoading === false ? (
+        <ImageGallery>
+          <ImageGalleryItem
+            pictures={pictures}
+            search_term={search_term}
+            openModal={openModal}
           />
-        )}
-      </>
-    );
-  }
-}
+        </ImageGallery>
+      ) : (
+        <div>
+          <Loader />
+        </div>
+      )}
+      {showButton && <Button loadMorePictures={loadMorePictures} />}
+      {modal.show && (
+        <Modal
+          closeModal={closeModal}
+          largeImage={modal.img}
+          alt={modal.imgAlt}
+        />
+      )}
+    </>
+  );
+};
